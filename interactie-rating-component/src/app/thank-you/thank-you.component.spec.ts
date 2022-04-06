7592515b2a9d9b6942f6e649b3596a82d65bea02
athira@voiro.com <kaithakkottuathira@gmail.com>
2022-04-06 18:03:35 +0530

    Thank you component Unit Test

--- interactie-rating-component/src/app/thank-you/thank-you.component.spec.ts
+++ interactie-rating-component/src/app/thank-you/thank-you.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ThankYouComponent } from './thank-you.component';
-import {Router} from "@angular/router";
+import {ActivatedRoute, convertToParamMap, Router} from "@angular/router";
 
 
 describe('ThankYouComponent', () => {
@@ -12,6 +12,12 @@ describe('ThankYouComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {snapshot: {params: {rating: '3'}}}
+        }
+      ],
       declarations: [ ThankYouComponent ]
     })
     .compileComponents();
@@ -25,5 +31,24 @@ describe('ThankYouComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get rating from routing ', () => {
+    expect(component.rating).toBe(3);
+  });
+
+  it('should display rating ', () => {
+    let rating=fixture.nativeElement.querySelector('.rating').children[0];
+    expect(rating.innerText).toBe(`You selected ${component.rating} out of 5`);
+  });
+  
+  it('should display heading', () => {
+    let h1 = fixture.nativeElement.querySelector('.heading');
+    expect(h1.textContent).toContain(component.heading);
+  });
+
+  it('should display paragraph', () => {
+    let text = fixture.nativeElement.querySelector('.text');
+    expect(text.textContent).toContain(component.text);
   });
 });
--- interactie-rating-component/src/app/thank-you/thank-you.component.ts
+++ interactie-rating-component/src/app/thank-you/thank-you.component.ts
@@ -8,6 +8,8 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ThankYouComponent implements OnInit {
   rating: number;
+  heading="Thank you!";
+  text="We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!";
   constructor(private route: ActivatedRoute) {
     this.rating = Number(this.route.snapshot.params.rating);
   }
